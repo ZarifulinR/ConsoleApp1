57418f131a6959c4b481532c599df82a849a454a
user <user@DESKTOP-5V689ON>
2023-05-01 14:28:42 +0300

    Task 1,2 complete

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,23 +1,59 @@
 ﻿using System;
 
+#region TASK_1
+//#region TASK_2
+
 namespace ConsoleApp2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите значение : ");
-            decimal a;
-            a = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Введите процент : ");
-            int b;
+            #region TASK_1
+            //Console.WriteLine("Введите число от 1 до 100");
+            //int a;
+            //a = Convert.ToInt32(Console.ReadLine());
 
-            b = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(a.GetType());
+            //if (a > 100 || a < 1)
+            //{
+            //    Console.WriteLine("Erorr");
 
-            Console.WriteLine(a.GetType());
-            Console.WriteLine(b.GetType());
+            //    Environment.Exit(0);
+            //}
+            //if (a % 3 == 0)
+            //{
+            //    Console.WriteLine("Fizz");
 
-            Console.WriteLine(System.Math.Round(a * b / 100));
+            //}
+
+            //if (a % 5 == 0)
+            //{
+            //    Console.WriteLine("Buzz");
+
+            //}
+            //if (a % 3 == 0 || a % 5 == 0)
+            //{
+            //    Console.WriteLine("FizzBizz");
+            //}
+            //else Console.WriteLine(a); 
+            #endregion
+
+            #region TASK_2
+            //Console.WriteLine("Введите значение : ");
+            //decimal a;
+            //a = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Введите процент : ");
+            //int b;
+
+            //b = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine(a.GetType());
+            //Console.WriteLine(b.GetType());
+
+            //Console.WriteLine(System.Math.Round(a * b / 100)); 
+            #endregion
+
 
         }
     }
