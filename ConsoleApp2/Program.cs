2f73c95754846b8b37022380f0b091e2e94a854b
user <user@DESKTOP-5V689ON>
2023-05-01 14:13:12 +0300

    HW_2

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -6,35 +6,18 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите число от 1 до 100");
-           int a ;
-            a = Convert.ToInt32(Console.ReadLine()); 
-           
-            Console.WriteLine (a.GetType());
-            if (a > 100 || a < 1)
-            {
-                Console.WriteLine("Erorr");
+            Console.WriteLine("Введите значение : ");
+            decimal a;
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите процент : ");
+            int b;
 
-             Environment.Exit(0);
-            }
-            if (a % 3 == 0)
-            {
-                Console.WriteLine("Fizz");
-               
-            }
-            
-            if (a%5==0)
-            {
-                Console.WriteLine("Buzz");
-               
-            }
-             if (a % 3 == 0 || a % 5 == 0)
-            {
-                Console.WriteLine("FizzBizz");
-            }
-            else Console.WriteLine(a);
-            
-            
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(a.GetType());
+            Console.WriteLine(b.GetType());
+
+            Console.WriteLine(System.Math.Round(a * b / 100));
 
         }
     }
