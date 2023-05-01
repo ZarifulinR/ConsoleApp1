b0a61b52f96690977e83efcadd74474be4fa185f
user <user@DESKTOP-5V689ON>
2023-05-01 13:45:17 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число от 1 до 100");
+            int a = 0;
+            Console.ReadLine(a);
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число от 1 до 100");
+           int a ;
+            a = Convert.ToInt32(Console.ReadLine()); 
+           
+            Console.WriteLine (a.GetType());
+            if (a > 100 || a < 1)
+            {
+                Console.WriteLine("Erorr");
+
+             Environment.Exit(0);
+            }
+            if (a % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+               
+            }
+            
+            if (a%5==0)
+            {
+                Console.WriteLine("Buzz");
+               
+            }
+             if (a % 3 == 0 || a % 5 == 0)
+            {
+                Console.WriteLine("FizzBizz");
+            }
+            else Console.WriteLine(a);
+            
+            
+
+        }
+    }
+}
